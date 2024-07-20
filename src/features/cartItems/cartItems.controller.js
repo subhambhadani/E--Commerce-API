f99a6457912e50ec3b35e159896b68be42a70ca5
Subham Bhadani <bhadanisubham@gmail.com>
2024-07-20 11:26:53 +0530

    create cart repo

--- src/features/cartItems/cartItems.controller.js
+++ src/features/cartItems/cartItems.controller.js
@@ -1,34 +1,57 @@
 
 import CartItemModel from "./cartItems.model.js";
+import CartItemsRepository from "./cartItems.repository.js";
 
 export class CartItemsController {
 
-    add(req, res) {
-        const { productID, quantity } = req.query;
-        const userID = req.userID;
-        CartItemModel.add(productID, userID, quantity)
-        res.status(201).send('Cart is updated');
+    constructor() {
+        this.cartItemsRepository = new CartItemsRepository();
     }
 
-    get(req, res) {
-        const userID = req.userID;
-        const items = CartItemModel.get(userID);
-        return res.status(200).send(items);
-    }
-
-    delete(req, res) {
-        const userID = req.userID;
-        const cartItemID = req.params.id;
-        const error = CartItemModel.delete(
-            cartItemID,
-            userID
-        );
-        if (error) {
-            return res.status(404).send(error);
+    async add(req, res) {
+        try {
+            const { productID, quantity } = req.body;
+            const userID = req.userID;
+            await this.cartItemsRepository.add(productID, userID, quantity)
+            res.status(201).send('Cart is updated');
+        } catch (err) {
+            console.log(err);
+            return res.status(404).send("Something went wrong");
         }
-        return res
-            .status(200)
-            .send('Cart item is removed');
+
+    }
+
+    async get(req, res) {
+        try {
+            const userID = req.userID;
+            const items = await this.cartItemsRepository.get(userID);
+            return res.status(200).send(items);
+        } catch (err) {
+            console.log(err);
+            return res.status(404).send("Something went wrong");
+        }
+
+    }
+
+    async delete(req, res) {
+        try {
+            const userID = req.userID;
+            const cartItemID = req.params.id;
+            const isDeleted = await this.cartItemsRepository.delete(
+                userID,
+                cartItemID,
+            );
+            if (!isDeleted) {
+                return res.status(404).send("Item not Found");
+            }
+            return res
+                .status(200)
+                .send('Cart item is removed');
+        } catch (err) {
+            console.log(err);
+            return res.status(404).send("Something went wrong");
+        }
+
     }
 
 }
---
+++ src/features/cartItems/cartItems.repository.js
@@ -0,0 +1,47 @@
+import { ObjectId } from "bson";
+import { getDB } from "../../config/mongodb.js";
+import { ApplicationError } from "../../error-handler/applicationError.js";
+
+
+export default class CartItemsRepository {
+
+    constructor() {
+        this.collection = "cartItmes";
+    }
+
+    async add(productID, userID, quantity) {
+        try {
+            // 1 . Get the db.
+            const db = getDB();
+            const collection = db.collection(this.collection);
+            await collection.insertOne({ productID: new ObjectId(productID), userID: new ObjectId(userID), quantity })
+        } catch (err) {
+            console.log(err);
+            throw new ApplicationError("Something went wrong with the database", 500)
+        }
+    }
+
+    async get(userID) {
+        try {
+            const db = getDB();
+            const collection = db.collection(this.collection);
+            return await collection.find({ userID: new ObjectId(userID) }).toArray();
+        } catch (err) {
+            console.log(err);
+            throw new ApplicationError("Something went wrong with the database", 500)
+        }
+    }
+
+    async delete(userID,cartItemID) {
+        try {
+            const db = getDB();
+            const collection = db.collection(this.collection);
+            const result = await collection.deleteOne({ _id: new ObjectId(cartItemID), userID: new ObjectId(userID) });
+            return result.deletedCount > 0;
+        } catch (err) {
+            console.log(err);
+            throw new ApplicationError("Something went wrong with the database", 500)
+        }
+    }
+
+}
